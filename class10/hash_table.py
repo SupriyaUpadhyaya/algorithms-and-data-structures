17c3f9bc76050a37ed9d9bce8ccb26e080f6548e
Supriya Upadhyaya <supriya.upadhyaya@st.ovgu.de>
2023-01-01 18:32:57 +0100

    Assignment 9.3 - hash table

--- class10/hash_table.py
+++ class10/hash_table.py
@@ -1,6 +1,8 @@
 from random import seed
+import random
 
 seed(0)
+capacity_ = 1249
 
 
 def h2(x: int) -> int:
@@ -13,14 +15,65 @@ class HashLinQuadDouble:
         self.table = [0 for _ in range(n)]
 
     def add_lin(self, obj: int) -> int:
-        raise NotImplementedError()  # TODO
+        length = len(self.table)
+        index = obj % length
+
+        position_checked = 1
+
+        while self.table[index] != 0:
+            if position_checked == (length - 1):
+                return position_checked
+            position_checked += 1
+            index = (index + 1) % length
+            
+        self.table[index] = obj
+
+        return position_checked
 
     def add_quad(self, obj: int) -> int:
-        raise NotImplementedError()  # TODO
+        length = len(self.table)
+        index = obj % length
+
+        position_checked = 1
+
+        while self.table[index] != 0:
+            if position_checked == (length - 1):
+                return position_checked
+            index = (obj + (position_checked * position_checked)) % length
+            position_checked += 1
+
+        self.table[index] = obj
+
+        return position_checked
 
     def add_double_hashing(self, obj: int) -> int:
-        raise NotImplementedError()  # TODO
+        length = len(self.table)
+        index = obj % length
+
+        position_checked = 1
+
+        while self.table[index] != 0:
+            if position_checked == (length - 1):
+                return position_checked
+            index = (obj + (h2(position_checked))) % length
+            position_checked += 1
+        self.table[index] = obj
+
+        return position_checked
 
 
 if __name__ == "__main__":
-    pass  # TODO: Compare the insertion functions
+    linear = HashLinQuadDouble(capacity_)
+    quadratic = HashLinQuadDouble(capacity_)
+    double = HashLinQuadDouble(capacity_)
+
+    print("Number      l_c    q_c    d_c")
+
+    for i in range(1000):
+        num = random.randint(1, 100000)
+        linear_collision = linear.add_lin(num)
+        quadratic_collision = quadratic.add_quad(num)
+        double_collision = double.add_double_hashing(num)
+        # print(num.__str__() + "       " + linear_collision.__str__() + 
+        #  "      " + quadratic_collision.__str__() 
+        # + "      " + double_collision.__str__())
